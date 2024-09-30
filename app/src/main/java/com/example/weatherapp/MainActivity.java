ac5af3f73839a28e966708d0e26183aa17bbe331
Agustin Fernandez <agusef22@gmail.com>
2024-09-30 14:54:28 -0300

    feat: add searchView with java

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.weatherapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.SearchView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,6 +32,31 @@ public class MainActivity extends AppCompatActivity {
 
         Button favoriteButton = findViewById(R.id.button);
         favoriteButton.setOnClickListener(this::onClick);
+
+        LinearLayout parentlayout = findViewById(R.id.parentLayout);
+
+        SearchView searchView = new SearchView(this);
+        LinearLayout.LayoutParams searchParams = new LinearLayout.LayoutParams(
+                700,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        searchView.setLayoutParams(searchParams);
+        searchView.setQueryHint("Search for any location");
+        searchView.setBackgroundResource(R.drawable.rounded_search_view);
+
+        ImageView imageView = new ImageView(this);
+        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(
+                105,
+                105
+        );
+        imageParams.gravity = Gravity.CENTER_VERTICAL;
+        imageParams.setMargins(25,0,0,0);
+        imageView.setLayoutParams(imageParams);
+        imageView.setImageResource(android.R.drawable.btn_star);
+
+        parentlayout.addView(searchView);
+        parentlayout.addView(imageView);
+
     }
 
     public void onClick(View view) {
