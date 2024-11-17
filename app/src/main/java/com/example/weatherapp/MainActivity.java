07c71bba6001fc341b874477b30698419bac1084
Agustin Fernandez <agusef22@gmail.com>
2024-11-17 18:00:03 -0300

    Feat: Add InfoActivity

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weatherapp;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
@@ -35,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView cityName;
     private TextView cityTemp;
     private ImageView imageCondition;
+    private TextView cityCondition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,12 +51,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
         weatherApiService = WeatherApiClient.getApiClient().create(WeatherApiService.class);
+        cityCondition = findViewById(R.id.textView);
         cityName = findViewById(R.id.textView2);
         cityTemp = findViewById(R.id.textView3);
         imageCondition = findViewById(R.id.imageView);
 
         Button favoriteButton = findViewById(R.id.button);
         favoriteButton.setOnClickListener(this::onClick);
+
+        Button InfoButton = findViewById(R.id.button11);
+        InfoButton.setOnClickListener(this::onClick);
+
 
         LinearLayout parentlayout = findViewById(R.id.parentLayout);
 
@@ -79,6 +87,14 @@ public class MainActivity extends AppCompatActivity {
         parentlayout.addView(searchView);
         parentlayout.addView(imageView);
 
+        imageCondition.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                ObjectAnimator rotate = ObjectAnimator.ofFloat(imageCondition, "rotation", 0f, 360f);
+                rotate.setDuration(500);
+                rotate.start();
+            }
+        });
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
             public boolean onQueryTextSubmit(String query) {
                 fetchWeatherData(query);
@@ -94,12 +110,24 @@ public class MainActivity extends AppCompatActivity {
 
     private void fetchWeatherData(String city) {
         String apikey = getString(R.string.api_key);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        imageCondition.setVisibility(View.GONE);
+        cityTemp.setVisibility(View.GONE);
+        cityName.setVisibility(View.GONE);
+        cityCondition.setVisibility(View.GONE);
 
         Call<WeatherData> call = weatherApiService.getWeather(apikey,city);
 
         call.enqueue(new Callback<WeatherData>() {
             @Override
             public void onResponse(Call<WeatherData> call, Response<WeatherData> response) {
+                progressBar.setVisibility(View.GONE);
+                imageCondition.setVisibility(View.VISIBLE);
+                cityTemp.setVisibility(View.VISIBLE);
+                cityName.setVisibility(View.VISIBLE);
+                cityCondition.setVisibility(View.VISIBLE);
+
                 if (response.isSuccessful() && response.body() != null) {
                     updateUI(response.body());
                 } else {
@@ -109,6 +137,12 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<WeatherData> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
+                imageCondition.setVisibility(View.VISIBLE);
+                cityTemp.setVisibility(View.VISIBLE);
+                cityName.setVisibility(View.VISIBLE);
+                cityCondition.setVisibility(View.VISIBLE);
+
                 Log.e("Mainactivity", "Error al obtener datos: " + t.getMessage());
                 Toast.makeText(MainActivity.this, "Error al obtener datos",Toast.LENGTH_SHORT).show();
             }
@@ -118,14 +152,25 @@ public class MainActivity extends AppCompatActivity {
     private void updateUI(WeatherData weatherData) {
         cityName.setText(weatherData.getLocation().getName());
         cityTemp.setText(String.format("%s°C", weatherData.getCurrent().getTemp_c()));
+        cityCondition.setText(weatherData.getCurrent().getCondition().getText());
 
         String iconURL = "https:" + weatherData.getCurrent().getCondition().getIcon();
         Glide.with(this).load(iconURL).into(imageCondition);
     }
 
     public void onClick(View view) {
-        Intent intent = new Intent(this, FavoriteActivity.class);
-        startActivity(intent);
+        Intent intent;
+
+        if(view.getId() == R.id.button) {
+            intent = new Intent(this, FavoriteActivity.class);
+            startActivity(intent);
+        }
+
+        if (view.getId() == R.id.button11) {
+            intent = new Intent(this, InfoActivity.class);
+            startActivity(intent);
+        }
+
     }
 
 
